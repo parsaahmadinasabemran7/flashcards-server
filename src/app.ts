889e08d474763b1719b1e8f3ab79389d36882be2
Parsa Ahmadi Nasab Emran <parsatempleuniversity@gmail.com>
2026-01-07 19:22:41 -0500

    feat(app): wire request logging into global middleware pipeline

--- src/app.ts
+++ src/app.ts
@@ -2,32 +2,22 @@ const express = require('express');
 
 const helmetMiddleware = require('./middleware/helmet');
 const corsMiddleware = require('./middleware/cors');
+const requestLogger = require('./middleware/requestLogger');
 const { generalLimiter } = require('./middleware/rateLimiter');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
-/**
- * Security hardening
- */
 app.disable('x-powered-by');
 
-/**
- * Global security middleware
- */
 app.use(helmetMiddleware);
 app.use(corsMiddleware);
+app.use(requestLogger);
 app.use(generalLimiter);
 
-/**
- * Body parsing
- */
 app.use(express.json({ limit: '100kb' }));
 app.use(express.urlencoded({ extended: false, limit: '100kb' }));
 
-/**
- * Error handling (must be last)
- */
 app.use(errorHandler);
 
 module.exports = app;
